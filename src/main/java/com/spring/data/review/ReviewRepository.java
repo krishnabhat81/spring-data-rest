82578beebf68d1ddce053e2ebbb3a358763a96bd
Krishna Bahadur Bhat <krishna.bhat78@gmail.com>
2017-05-01 13:28:01 -0400

    API Security: Authorizing review deletion

--- src/main/java/com/spring/data/review/ReviewRepository.java
+++ src/main/java/com/spring/data/review/ReviewRepository.java
@@ -2,7 +2,9 @@ package com.spring.data.review;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Created by krishna1bhat on 4/30/17.
@@ -10,5 +12,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
-    
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.username == authentication.name")
+    void delete(@Param("review") Review entity);
 }
