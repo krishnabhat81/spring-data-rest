abed12036b8c3798e8e6fad0e6c160d308b53330
Krishna Bahadur Bhat <krishna.bhat78@gmail.com>
2017-04-30 23:30:58 -0400

    Versioning: Entity (check BaseEntity) and API(application.properties)

--- src/main/java/com/spring/data/core/BaseEntity.java
+++ src/main/java/com/spring/data/core/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.spring.data.core;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 /**
  * Created by krishna1bhat on 4/30/17.
@@ -14,6 +11,16 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+
+    //this is for Entity versioning
+    //to check
+    //get request any api url: check the header, you will see ETag = 0 first time
+    //then if you PUT(add new record for same),  check header => ETag = 1
+    //currently ETag is 1
+    //if you request GET sending ETag = 1 you will get 304 status code
+    //you can use this concept in different apps like android, ios, web app....
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;
