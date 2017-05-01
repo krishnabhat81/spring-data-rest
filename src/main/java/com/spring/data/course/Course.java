d8a5507d8294cdd7148f0ec6ac8ebbbac33aa99a
Krishna Bahadur Bhat <krishna.bhat78@gmail.com>
2017-05-01 00:21:29 -0400

    API validation: validating Course Entity title -- before entity is created or saved (look RestConfig)

--- src/main/java/com/spring/data/course/Course.java
+++ src/main/java/com/spring/data/course/Course.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +19,8 @@ import java.util.List;
 @Data
 @Entity
 public class Course extends BaseEntity {
+    @NotNull
+    @Size(min = 2, max = 140)
     private String title;
     private String url;
 
